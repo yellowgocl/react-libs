70dabf7b16ccbd1086c1f871e53861a988d3e7eb
got.wang <got@zhibankeji.com>
2021-01-11 19:35:34 +0800

    e

--- packages/lint-config/index.js
+++ packages/lint-config/index.js
@@ -1,7 +1,22 @@
 module.exports = {
- 
+  parser: "babel-eslint",
+  extends: ["airbnb"],
+  plugin: ["react", "react-hooks", "import"],
   env: {
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      legacyDecorators: true,
+      modules: true,
+      jsx: true,
+    },
+  },
   
+  rules: {
+    "space-before-function-paren": 0,
+    indent: [2, 2, { VariableDeclarator: 1 }],
+  },
 };
